c351b004fdb9045c362f3462f25986d4e41714fe
Emiliano Carreño <emiliano0662@gmail.com>
2018-08-02 23:54:02 -0500

    cambios generales

--- src/js/main.js
+++ src/js/main.js
@@ -5,58 +5,9 @@ $(document).ready(function() {
 		margin: 0,
 		loop: true,
 		nav: false,
-		dots: false,
-		autoplay: true
-	});
-
-	$(".btn-owl-carousel-main.btn-left").on('click', function (event) {
-		event.preventDefault();
-
-		owl_carousel_main.trigger('prev.owl.carousel');
-	});
-
-	$(".btn-owl-carousel-main.btn-right").on('click', function (event) {
-		event.preventDefault();
-
-		owl_carousel_main.trigger('next.owl.carousel');
-	});
-
-	$(".btn-dropdown-menu-create-story").on('click', function (event) {
-		event.preventDefault();
-
-		var language = $(this).data('language');
-
-		$('#input-create-story-language').val(language);
-	});
-
-	$("#form-contact").submit(function (event) {
-		event.preventDefault();
-
-		$.ajax({
-			cache: false,
-			type: $(this).attr("method"),
-			url: $(this).attr("action"),
-			data: $(this).serialize(),
-			success: function (data) {
-
-				if (data) {
-
-					$('.alert').hide();
-					$('.alert-success').fadeIn();
-
-					$("#form-contact")[0].reset();
-					
-				}else{
-
-					$('.alert').hide();
-					$('.alert-danger').fadeIn();
-
-				}
-
-				setTimeout(function () { $('.alert').hide(); }, 5000);
-			}
-		});
-
+		dots: true,
+		autoplay: true,
+		animateOut: 'fadeOut'
 	});
 
 	$("[data-dinaanim]").each(function () {
